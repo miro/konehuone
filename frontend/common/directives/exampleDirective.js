b067ed55465e777bc84fba1f32de84274a8a7996
Miro Nieminen <miro.nieminen@futurice.com>
2015-10-13 22:33:47 +0300

    Remove example service & directive

--- frontend/common/directives/exampleDirective.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('ngDevstack.myDirectiveModule', [])
-
-.directive('myDirective', function () {
-    return {
-        // directive code
-    };
-});
--- frontend/common/services/exampleService.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('ngDevstack.myServiceModule', [])
-
-.factory('myService', function () {
-    return {
-        // service data
-    };
-});
